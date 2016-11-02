5b9f3bd65a5a85cf5763c1a8f1467fa53e419a0b
neossoftware <mario.hidalgom@yahoo.com.mx>
2016-11-01 18:58:24 -0600

    gulp task

--- chapter3/gulpfile.js
+++ chapter3/gulpfile.js
@@ -12,25 +12,38 @@ gulp.task('copy-index', function() {
 
 //copy index.html to dist folder
 gulp.task('copy-index', function() {
-   return gulp
-     .src('index.html')
-     .pipe(gulp.dest('dist'));
+    console.log('copy index task');
+    return gulp
+        .src('index.html')
+        .pipe(gulp.dest('dist'));
 });
 
 //  .src('images/**/*.{png,jpg}') copy n level in image folder
 
 gulp.task('images', function() {
-  return gulp
-      .src('images/**/*.{png,jpg}') //in this case it doesnt have spaces ;)
-      .pipe(gulp.dest('dist/images'));
+    console.log('images task');
+    return gulp
+        .src('images/**/*.{png,jpg}') //in this case it doesnt have spaces ;)
+        .pipe(gulp.dest('dist/images'));
 });
 
 //copy /exclude
 
 gulp.task('data', function() {
-     return gulp
-       .src(['json/*.json','xml/*.xml','!json/*-secret.json'])
-       .pipe(gulp.dest('dist/data'))       
+    console.log('data task');
+    return gulp
+        .src(['json/*.json', 'xml/*.xml', '!json/*-secret.json'])
+        .pipe(gulp.dest('dist/data'))
 });
 
-gulp.task('default',['copy-index', 'images', 'data']);
+gulp.task('watch', function() {
+    console.log('execute watch task');
+    gulp.watch('index.html', ['copy-index']); //verifica el estado de
+
+    gulp.watch('images/**/*.{png,jpg}', ['images']);
+
+    gulp.watch(['json/*.json', 'xml/*.xml', '!json/*-secret.json'], ['data']);
+    //index.html
+})
+
+gulp.task('default', ['copy-index', 'images', 'data']);
